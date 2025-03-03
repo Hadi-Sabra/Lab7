71fcd887cbb3b44cb3cdc8b364ecb589a7f5db62
Hadi-Sabra <131852949+Hadi-Sabra@users.noreply.github.com>
2025-03-03 11:04:57 +0200

    Added new Micro Service Files

---
+++ EnrollmentService/Data/EnrollmentDbContext.cs
@@ -0,0 +1,13 @@
+﻿using EnrollmentService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EnrollmentService.Data
+{
+    public class EnrollmentDbContext : DbContext
+    { 
+        public EnrollmentDbContext(DbContextOptions<EnrollmentDbContext> options) : base(options) { }
+
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+    }
+}
